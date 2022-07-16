57135d597368c1bc72301174fe75e3091662c3f9
Mohitur <mohitur669@gmail.com>
2022-07-16 12:52:15 +0530

    Pattern Complete

--- Patterns/squareNumber.java
+++ Patterns/squareNumber.java
@@ -1,16 +1,21 @@
-
-
+//        4 4 4 4 4 4 4
+//        4 3 3 3 3 3 4
+//        4 3 2 2 2 3 4
+//        4 3 2 1 2 3 4
+//        4 3 2 2 2 3 4
+//        4 3 3 3 3 3 4
+//        4 4 4 4 4 4 4
 public class squareNumber {
     public static void main(String[] args) {
-//        int n = 4;
-//        int originalN = n;
-//        n = 2 * n;
-//        for (int row = 0; row <= n; row++) {
-//            for (int col = 0; col <= n; col++) {
-//                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
-//                System.out.print(atEveryIndex + " ");
-//            }
-//            System.out.println();
-//        }
+        int n = 4;
+        int N = 2 * n - 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int minDistance = Math.min(Math.min(i, j), Math.min(N - i - 1, N - j - 1));
+                int val = n - minDistance;
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
     }
 }
