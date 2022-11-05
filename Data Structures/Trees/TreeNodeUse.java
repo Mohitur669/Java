78358e03c607577aa5f7ba7204a0f2e9d1f913d0
Mohitur669 <mohitur669@gmail.com>
2022-11-05 17:41:39 +0530

    test2

--- Data Structures/Trees/TreeNodeUse.java
+++ Data Structures/Trees/TreeNodeUse.java
@@ -1,16 +1,49 @@
+import java.util.Scanner;
+
 public class TreeNodeUse {
     public static void main(String[] args) {
-        TreeNode<Integer> root = new TreeNode<>(4);
-        TreeNode<Integer> node1 = new TreeNode<>(2);
-        TreeNode<Integer> node2 = new TreeNode<>(3);
-        TreeNode<Integer> node3 = new TreeNode<>(5);
-        TreeNode<Integer> node4 = new TreeNode<>(6);
+        Scanner s = new Scanner(System.in);
+        TreeNode<Integer> root = takeInput(s);
+        print(root);
 
-        root.children.add(node1);
-        root.children.add(node2);
-        root.children.add(node3);
-        node2.children.add(node4);
+//        TreeNode<Integer> root = new TreeNode<>(4);
+//        TreeNode<Integer> node1 = new TreeNode<>(2);
+//        TreeNode<Integer> node2 = new TreeNode<>(3);
+//        TreeNode<Integer> node3 = new TreeNode<>(5);
+//        TreeNode<Integer> node4 = new TreeNode<>(6);
+//
+//        root.children.add(node1);
+//        root.children.add(node2);
+//        root.children.add(node3);
+//        node2.children.add(node4);
+//
+//        System.out.println(root);
+    }
 
-        System.out.println(root);
+    // taking tree input from the user - recursively
+    public static TreeNode<Integer> takeInput(Scanner s) {
+        int n;
+        System.out.println("Enter next node data: ");
+        n = s.nextInt();
+        TreeNode<Integer> root = new TreeNode<>(n);
+        System.out.println("Enter number of children for " + n);
+        int childCount = s.nextInt();
+        for (int i = 0; i < childCount; i++) {
+            TreeNode<Integer> child = takeInput(s);
+            root.children.add(child);
+        }
+        return root;
+    }
+
+    // printing the tree taken from the user - recursively
+    public static void print(TreeNode<Integer> root) {
+        String s = root.data + ":";
+        for (int i = 0; i < root.children.size(); i++) {
+            s = s + root.children.get(i).data + ",";
+        }
+        System.out.println(s);
+        for (int i = 0; i < root.children.size(); i++) {
+            print(root.children.get(i));
+        }
     }
 }
