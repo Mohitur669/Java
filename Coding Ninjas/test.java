2935e67723ac17e1b4bc76ae72bfa478f5ab1700
Mohitur669 <mohitur669@gmail.com>
2022-11-05 11:56:38 +0530

    2 laptop setup

--- Coding Ninjas/test.java
+++ Coding Ninjas/test.java
@@ -4,10 +4,6 @@ import java.util.Stack;
 // this is a testing code
 public class test {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.push(5);
-        stack.push(10);
-        stack.push(15);
-        System.out.print(stack.pop() + stack.size());
+        System.out.println("this is a test file");
     }
 }
