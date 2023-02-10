721cadb531961341e91734385c9acc66843461ef
Mohitur669 <mohitur669@gmail.com>
2023-02-11 02:56:46 +0530

    modify insertion sort

--- Algorithms/Insertion Sort/insertion.java
+++ Algorithms/Insertion Sort/insertion.java
@@ -11,10 +11,7 @@ public class insertion {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
-//                    swap(arr, j, j - 1);
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    swap(arr, j, j - 1);
                 } else {
                     break;
                 }
@@ -22,9 +19,9 @@ public class insertion {
         }
     }
 
-//    static void swap(int[] arr, int first, int second) {
-//        int temp = arr[first];
-//        arr[first] = arr[second];
-//        arr[second] = temp;
-//    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
