c9186aa1b86973863296685cc8f7cf74305dd464
Mohitur669 <mohitur669@gmail.com>
2023-05-25 00:18:44 +0530

    final

--- Coding Ninjas/arrayFindUnique.java
+++ Coding Ninjas/arrayFindUnique.java
@@ -5,18 +5,18 @@ public class arrayFindUnique {
         System.out.println(unique);
     }
 
-    public static int findUnique(int[] arr){
+    public static int findUnique(int[] arr) {
         int n = arr.length;
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             int j;
-            for(j=0;j<n;j++){
-                if(i!=j){
-                    if(arr[i]==arr[j]){
+            for (j = 0; j < n; j++) {
+                if (i != j) {
+                    if (arr[i] == arr[j]) {
                         break;
                     }
                 }
             }
-            if(j==n)
+            if (j == n)
             //means we check i for every j and
             //found no match for that i value,
             //showing that arr[i] is unique
