67aee0e6a2b7194571113b78d475a1e8954abb24
Mohitur669 <mohitur669@gmail.com>
2023-06-27 07:22:14 +0530

    home

--- LeetCode/Important/BrainTeasers.java
+++ LeetCode/Important/BrainTeasers.java
@@ -1,5 +1,8 @@
 public class BrainTeasers {
     public static void main(String[] args) {
         System.out.println("This is a Brain Teaser code test-runner.");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
     }
 }
